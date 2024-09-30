14d58d7ae19dca0beae6e7bbe05defb06f23fa64
Christine Chen <zhzhnaicha2@gmail.com>
2024-09-30 03:15:40 -0400

    wrote test cases for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,15 +1,35 @@
 public class MyArrays {
-public static String aryToString(int[] nums) {
-  String aString = "[";
-  for (int i=0; i < nums.length && nums.length > 0; i++) {
-    aString += nums[i];
-    if (i != nums.length -1) {
-      aString += ", ";
-    }
-  }
-  return aString + "]";
-}
 
-  public static void main(String[] args)
-// test cases for returnCopy
-System.out.println()
+  public static String aryToString(int[] nums) {
+    String aString = "[";
+    for (int i=0; i < nums.length && nums.length > 0; i++) {
+      aString += nums[i];
+      if (i != nums.length -1) {
+        aString += ", ";
+      }
+    }
+    return aString + "]";
+  }
+
+  public static int[] returnCopy(int[] ary) {
+  }
+
+  public static int[] concatArray(int[] ary1, int[] ary2) {
+  }
+
+  public static void main(String[] args) {
+
+  // test cases for returnCopy
+    int[] original = {1, 2, 3, 4, 5};
+    int[] copy = returnCopy(original);
+    System.out.println("returnCopy - Original: " + aryToString(original));
+    System.out.println("returnCopy - Copy: " + aryToString(copy));
+    System.out.println("returnCopy - Different address: " + (original != copy));
+
+    int[] original = {};
+    int[] copy = returnCopy(original);
+    System.out.println("returnCopy - Original: " + aryToString(original));
+    System.out.println("returnCopy - Copy: " + aryToString(copy));
+    System.out.println("returnCopy - Different address: " + (original != copy));
+
+  }
