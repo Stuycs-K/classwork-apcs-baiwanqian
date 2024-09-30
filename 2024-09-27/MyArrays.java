4eba9495bc7f971dad91c4ad70714355f1261d2b
Christine Chen <zhzhnaicha2@gmail.com>
2024-09-30 03:25:47 -0400

    completed test cases for concatArray and added additional for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,6 @@
 public class MyArrays {
 
-  public static String aryToString(int[] nums) {
+  public static String arrayToString(int[] nums) {
     String aString = "[";
     for (int i=0; i < nums.length && nums.length > 0; i++) {
       aString += nums[i];
@@ -22,14 +22,34 @@ public class MyArrays {
   // test cases for returnCopy
     int[] original = {1, 2, 3, 4, 5};
     int[] copy = returnCopy(original);
-    System.out.println("returnCopy - Original: " + aryToString(original));
-    System.out.println("returnCopy - Copy: " + aryToString(copy));
-    System.out.println("returnCopy - Different address: " + (original != copy));
+    System.out.println("Original: " + arrayToString(original));
+    System.out.println("Copy: " + arrayToString(copy));
+    System.out.println("Different address: " + (original != copy));
 
     int[] original = {};
     int[] copy = returnCopy(original);
-    System.out.println("returnCopy - Original: " + aryToString(original));
-    System.out.println("returnCopy - Copy: " + aryToString(copy));
-    System.out.println("returnCopy - Different address: " + (original != copy));
+    System.out.println("Original: " + arrayToString(original));
+    System.out.println("Copy: " + arrayToString(copy));
+    System.out.println("Different address: " + (original != copy));
 
-  }
+    int[] original = {1};
+    int[] copy = returnCopy(original);
+    System.out.println("Original: " + arrayToString(original));
+    System.out.println("Copy: " + arrayToString(copy));
+    System.out.println("Different address: " + (original != copy));
+
+  // test cases for concatArray
+    int[] ary1 = {1, 2, 3};
+    int[] ary2 = {4, 5, 6};
+    int[] concatenated = concatArray(ary1, ary2);
+    System.out.println("Concatenated Arrays: " + aryToString(concatenated));
+
+    int[] ary2 = {};
+    int[] concatenated = concatArray(ary1, ary2);
+    System.out.println("Concatenated Arrays: " + aryToString(concatenated));
+
+    int[] ary1 = {};
+    int[] concatenated = concatArray(ary1, ary2);
+    System.out.println("Concatenated Arrays: " + aryToString(concatenated));
+
+}
