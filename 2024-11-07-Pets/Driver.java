9ef29ebe6397b6d3fe4dd1f7f9137d3899ec5ddb
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-07 09:57:18 -0600

    tested which types can be assigned and commented out error producing one

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,9 +1,14 @@
 public class Driver{
   public static void main(String[] args){
-    Animal a1 =  new Animal("moo", 3, "cow");
-    a1.speak();
+    Animal cow =  new Animal("moo", 3, "cow");
+    cow.speak();
 
-    Bird b1 = new Bird("cacow", 2, "bird", 4.0, "yellow");
-    b1.speak();
+    Bird bird_test = new Bird("cacow", 2, "bird", 4.0, "yellow");
+    bird_test.speak();
+
+    Animal a1 = new Animal("woof", 1, "dog");
+    Bird b1 = new Bird("chirp", 9, "songbird", 2.0, "pink");
+    // Bird b2 = new Animal("ribbit", 5, "frog");
+    Animal a2 = new Bird("quack", 7, "duck", 8.0, "golden");
   }
 }
