66a264a08095e2b554b69e38b5cbba749a7d94e4
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-19 03:22:57 -0500

    proper imports, fixed to iterate over steps and check each step

--- 2014-11-18-advent2016/D1P2.java
+++ 2014-11-18-advent2016/D1P2.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class D1P2 {
 
@@ -11,6 +12,8 @@ public class D1P2 {
       Scanner input = new Scanner(file);
       String[] instructions = input.nextLine().split(", ");
       int x = 0, y = 0, direction = 0;
+      ArrayList<String> visited = new ArrayList<String>();
+      visited.add("0,0");
       for (int i = 0; i < instructions.length; i++) {
         if (instructions[i].charAt(0) == 'R') {
           direction = (direction + 1) % 4;
@@ -21,21 +24,22 @@ public class D1P2 {
         for (int j = 1; j < instructions[i].length(); j++) {
           steps = steps * 10 + (instructions[i].charAt(j) - '0');
         }
-        if (direction == 0) {
-          y += steps;
-        } else if (direction == 1) {
-          x += steps;
-        } else if (direction == 2) {
-          y -= steps;
-        } else if (direction == 3) {
-          x -= steps;
-        }
-        String current = x + "," + y;
-        ArrayList<String> visited = new ArrayList<String>();
-        visited.add("0,0");
-        if (visited.contains(current)) {
-          System.out.println(Math.abs(x) + Math.abs(y));
-          return;
+        for (int j = 1; j <= steps; j++) {
+          if (direction == 0) {
+            y++;
+          } else if (direction == 1) {
+            x++;
+          } else if (direction == 2) {
+            y--;
+          } else if (direction == 3) {
+            x--;
+          }
+          String current = x + "," + y;
+          if (visited.contains(current)) {
+            System.out.println(Math.abs(x) + Math.abs(y));
+            return;
+          }
+          visited.add(current);
         }
       }
     } catch (FileNotFoundException ex) {
