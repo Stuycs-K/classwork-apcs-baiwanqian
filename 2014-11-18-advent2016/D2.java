6968b38d2d6ed8ff125ea8819b0d872c91999bb4
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-19 18:21:38 -0500

    completed code (working)

--- 2014-11-18-advent2016/D2.java
+++ 2014-11-18-advent2016/D2.java
@@ -1,35 +1,41 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class D2 {
 
   public static void main(String[] args) {
     String filename = "D2Input.txt";
-    ArrayList<Character> = new ArrayList<Character< {
-      {'1', '2', '3'}
-      {'4', '5', '6'}
-      {'7', '8', '9'}
+    int[][] keypad = new int[][]{
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
     };
-    int x = 1, y = 1;
+    int y = 1, x = 1;
+    String passcode = "";
     try {
-      File file = new file(filename);
-      Scanner input = new scanner(file);
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
       while (input.hasNextLine()) {
-        dir = input.nextLine();
-        for (int i = 0; i < dir.length(); i++)
-          if (charAt(i) == 'U') {
-            if x
+        String dir = input.nextLine();
+        for (int i = 0; i < dir.length(); i++) {
+          char shift = dir.charAt(i);
+          if (shift == 'U') {
+            if (y > 0) y--;
+          } else if (shift == 'D') {
+            if (y < 2) y++;
+          } else if (shift == 'R') {
+            if (x < 2) x++;
+          } else if (shift == 'L') {
+            if (x > 0) x--;
           }
-          }
+        }
+        passcode += keypad[y][x];
       }
-
-      }
-
-
+      System.out.println(passcode);
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
     }
+  }
 
 }
