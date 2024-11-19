7e18cb9b99ed26053767c6424e89a1307027d068
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-19 11:01:19 -0500

    started writing code

---
+++ 2014-11-18-advent2016/D2.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class D2 {
+
+  public static void main(String[] args) {
+    String filename = "D2Input.txt";
+    ArrayList<Character> = new ArrayList<Character< {
+      {'1', '2', '3'}
+      {'4', '5', '6'}
+      {'7', '8', '9'}
+    };
+    int x = 1, y = 1;
+    try {
+      File file = new file(filename);
+      Scanner input = new scanner(file);
+      while (input.hasNextLine()) {
+        dir = input.nextLine();
+        for (int i = 0; i < dir.length(); i++)
+          if (charAt(i) == 'U') {
+            if x
+          }
+          }
+      }
+
+      }
+
+
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+
+}
