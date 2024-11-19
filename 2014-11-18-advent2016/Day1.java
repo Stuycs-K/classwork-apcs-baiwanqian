2c0e8d59f6d06deaeeefe9c24292d32d605e7afc
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-19 02:36:50 -0500

    finished writing code

--- 2014-11-18-advent2016/Day1.java
+++ 2014-11-18-advent2016/Day1.java
@@ -9,21 +9,34 @@ public class Day1 {
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
-      String[] instructions = input.nextLine().split(", ")
+      String[] instructions = input.nextLine().split(", ");
       int x, y, direction = 0;
-      while (input.hasNext()) {
-        if(input.next().charAt(0) == 'R') {
-          direction += 90;
+      for (int i = 0; i < instructions.length(); i++) {
+        if (instructions[i].charAt(0) == 'R') {
+          direction = (direction + 1) % 4;
         } else {
-          direction -= 90;
-    }
-
+          direction = (direction + 3) % 4;
+        }
+      int steps = 0;
+      for (int j = 1; j < instructions[i].length(); j++) {
+        steps = steps * 10 + (instructions[i].charAt(j) - '0');
+      }
+      if (direction == 0) {
+        y += steps;
+      }
+      if (direction == 1) {
+        x += steps;
+      }
+      if (direction == 2) {
+        y -= steps;
+      }
+      if (direction == 3) {
+        x -= steps;
       }
     }
-    catch {
-
-    }
+    System.out.println(Math.abs(x) + Math.abs(y));
+  } catch (FileNotFoundException ex) {
+    System.out.println("File not found");
   }
-
-
+}
 }
