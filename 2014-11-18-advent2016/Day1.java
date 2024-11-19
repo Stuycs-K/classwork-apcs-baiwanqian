ed06b8626db1dd761cbd1a8545479a604a41c11e
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-19 02:43:44 -0500

    fixed () on length, closing brackets, and initialization of x and y

--- 2014-11-18-advent2016/Day1.java
+++ 2014-11-18-advent2016/Day1.java
@@ -10,33 +10,31 @@ public class Day1 {
       File file = new File(filename);
       Scanner input = new Scanner(file);
       String[] instructions = input.nextLine().split(", ");
-      int x, y, direction = 0;
-      for (int i = 0; i < instructions.length(); i++) {
+      int x = 0, y = 0, direction = 0;
+      for (int i = 0; i < instructions.length; i++) {
         if (instructions[i].charAt(0) == 'R') {
           direction = (direction + 1) % 4;
         } else {
           direction = (direction + 3) % 4;
         }
-      int steps = 0;
-      for (int j = 1; j < instructions[i].length(); j++) {
-        steps = steps * 10 + (instructions[i].charAt(j) - '0');
+        int steps = 0;
+        for (int j = 1; j < instructions[i].length(); j++) {
+          steps = steps * 10 + (instructions[i].charAt(j) - '0');
+        }
+        if (direction == 0) {
+          y += steps;
+        } else if (direction == 1) {
+          x += steps;
+        } else if (direction == 2) {
+          y -= steps;
+        } else if (direction == 3) {
+          x -= steps;
+        }
       }
-      if (direction == 0) {
-        y += steps;
-      }
-      if (direction == 1) {
-        x += steps;
-      }
-      if (direction == 2) {
-        y -= steps;
-      }
-      if (direction == 3) {
-        x -= steps;
-      }
+      System.out.println(Math.abs(x) + Math.abs(y));
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
     }
-    System.out.println(Math.abs(x) + Math.abs(y));
-  } catch (FileNotFoundException ex) {
-    System.out.println("File not found");
   }
-}
+
 }
