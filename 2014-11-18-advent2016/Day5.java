2f13e93422e23cdd01803c1df1a1d2464dc1f7cf
Christine Chen <zhzhnaicha2@gmail.com>
2024-11-20 02:58:52 -0500

    completed day 5 part 1

---
+++ 2014-11-18-advent2016/Day5.java
@@ -0,0 +1,53 @@
+import java.security.*;
+
+public class Day5 {
+// really slow :(
+  public static String retrievePassword(String doorID) {
+    char[] password = new char[8];
+    int index = 0;
+    int count = 0;
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+        while (count < 8) {
+          byte[] array = md.digest((doorID + index).getBytes("UTF-8"));
+          // convert to hexadecimal string
+          String hex = bytesToHex(array);
+          if (hex.substring(0,5).equals("00000")) {
+            password[count] = hex.charAt(5);
+            count++;
+        }
+        index++;
+      }
+    } catch (Exception e) {
+      System.out.println("Error " + e);
+    }
+    return new String(password);
+  }
+
+  public static String bytesToHex(byte[] bytes) {
+    String hexString = "0123456789abcdef";
+    String hexResult = "";
+    for (int i = 0; i < bytes.length; i++) {
+      // convert to unsigned byte (between 0 to 255 instead of -128 to 127)
+      int unsignedByte;
+      if (bytes[i] < 0) {
+        unsignedByte = bytes[i] + 256;
+      } else {
+        unsignedByte = bytes[i];
+      }
+      // upper 4 bits
+      int upper = unsignedByte / 16;
+      // lower 4 bits
+      int lower = unsignedByte % 16;
+      hexResult += hexString.charAt(upper);
+      hexResult += hexString.charAt(lower);
+    }
+    return hexResult;
+  }
+
+  public static void main(String[] args) {
+    String doorID = "cxdnnyjw";
+    System.out.println(retrievePassword(doorID));
+  }
+
+}
