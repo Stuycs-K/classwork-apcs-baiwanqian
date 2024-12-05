692c716724df3ced049b33662a120da2438ba6d5
Christine Chen <zhzhnaicha2@gmail.com>
2024-12-04 19:05:41 -0500

    wrote function to create random int array and displayed it

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class Driver {
 
   public static void drawBorder(Text text) {
@@ -19,7 +17,36 @@ public class Driver {
     text.resetColor();
   }
 
-  public static void randomInteger
+  public static int[] createRandomArray(int size) {
+    int[] randArray = new int[size];
+    for (int i = 0; i < size; i++) {
+      randArray[i] = (int) (Math.random() * 100);
+    }
+    return randArray;
+  }
+
+  public static void displayRandom(Text text, String[] randArray) {
+    int position = 20;
+    for (int i = 0; i < randArray.length; i++) {
+      int num = randArray[i];
+      if (num < 25) {
+        text.setForegroundColor(1);
+      } else if (num > 75) {
+        text.setForegroundColor(2);
+      } else {
+        text.resetColor();
+      }
+      text.moveCursor(2, positon);
+      text.print(value);
+    }
+    position += 20;
+    text.resetColor();
+  }
+
+  public static void drawHorizontalSeperator(Text text) {
+    text.setBackgroundColor(4);
+    for (int i = )
+  }
 
   public static void main(String[] args) {
 
