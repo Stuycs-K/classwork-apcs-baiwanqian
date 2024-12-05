39d637a97503d68cd1bda720b30fb2285c71b5df
Christine Chen <zhzhnaicha2@gmail.com>
2024-12-05 01:25:34 -0500

    fixed methods to properly use text class (I started by creating own methods, then forgot to change names when using text class at first!)

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,20 +1,20 @@
 public class Driver {
 
-  public static void drawBorder(Text text) {
-    text.setBackgroundColor(4);
-    for (int i = 1; i <=80; i++) {
-      text.moveCursor(1, i);
-      text.print(" ");
-      text.moveCursor(30, i);
-      text.print(" ");
+  public static void drawBorder() {
+    Text.color(Text.BRIGHT, Text.BLUE, Text.background(Text.CYAN));
+    for (int i = 1; i <= 80; i++) {
+      Text.go(1, i);
+      System.out.print(" ");
+      Text.go(30, i);
+      System.out.print(" ");
     }
     for (int i = 2; i <= 29; i++) {
-      text.moveCursor(i, 1);
-      text.print(" ");
-      text.moveCursor(i, 80);
-      text.print(" ");
+      Text.go(i, 1);
+      System.out.print(" ");
+      Text.go(i, 80);
+      System.out.print(" ");
     }
-    text.resetColor();
+    System.out.print(Text.RESET);
   }
 
   public static int[] createRandomArray(int size) {
@@ -25,46 +25,55 @@ public class Driver {
     return randArray;
   }
 
-  public static void displayRandom(Text text, String[] randArray) {
+  public static void displayRandom(int[] randArray) {
     int position = 20;
-    for (int i = 0; i < randArray.length; i++) {
-      int num = randArray[i];
+    for (int num : randArray) {
       if (num < 25) {
-        text.setForegroundColor(1);
+        Text.color(Text.BRIGHT, Text.RED);
       } else if (num > 75) {
-        text.setForegroundColor(2);
+        Text.color(Text.BRIGHT, Text.GREEN);
       } else {
-        text.resetColor();
+        Text.color(Text.BRIGHT, Text.WHITE);
       }
-      text.moveCursor(2, positon);
-      text.print(value);
+      Text.go(2, position);
+      System.out.print(String.valueOf(num));
+      position += 20;
     }
-    position += 20;
-    text.resetColor();
+    System.out.print(Text.RESET);
   }
 
-  public static void drawHorizontalSeperator(Text text) {
-    text.setBackgroundColor(4);
+  public static void drawHorizontalSeperator() {
+    Text.color(Text.BRIGHT, Text.BLACK, Text.background(Text.CYAN));
     for (int i = 1; i <= 80; i++) {
-      text.moveCursor(3, i);
-      text.print("-");
+      Text.go(3, i);
+      System.out.print("-");
     }
-    text.resetColor();
+    System.out.print(Text.RESET);
   }
 
-  public static void drawInMiddle(Text text) {
-    text.moveCursor(15, 35);
-    text.print("^ ^");
+  public static void drawInMiddle() {
+    int x = 15;
+    int y = 40;
+    Text.go(x, y);
+    System.out.print("^ ^");
+    Text.go(x + 1, y);
+    System.out.print("o.o");
+    Text.go(x + 2, y - 1);
+    System.out.print("\\| |/");
+    Text.go(x + 3, y);
+    System.out.print("| |");
   }
 
   public static void main(String[] args) {
-    Text text = new Text();
-    text.clearScreen();
-    drawBorder(text);
-    displayRandom(text);
-    drawHorizontalSeperator(text);
-    drawInMiddle(text);
-    text.moveCursor(31, 1);
+    System.out.print(Text.CLEAR_SCREEN);
+    System.out.print(Text.HIDE_CURSOR);
+    drawBorder();
+    int[] randomNums = createRandomArray(3);
+    displayRandom(randomNums);
+    drawHorizontalSeperator();
+    drawInMiddle();
+    Text.go(31, 1);
+    System.out.print(Text.RESET);
   }
 
 }
