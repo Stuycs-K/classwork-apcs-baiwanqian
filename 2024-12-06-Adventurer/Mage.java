dff15fc15a1f8689a91f6fc2383977877fc67633
Christine Chen <zhzhnaicha2@gmail.com>
2024-12-06 02:06:33 -0500

    wrote support other, support, and specialAttack

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -5,7 +5,7 @@ public class Mage extends Adventurer {
   public Mage(String name) {
     super(name, 10);
     this.mana = 10;
-    this.maxMana = 20;
+    this.maxMana = 15;
   }
 
   public String getSpecialName() {
@@ -23,8 +23,30 @@ public class Mage extends Adventurer {
   public String attack(Adventurer other) {
     int damage = 2;
     other.applyDamage(damage);
-    return this.getName() + " chucks a spell on " + other.getName() + " for " + damage + " damage.";
+    return this.getName() + " chucks a spell on " + other.getName() + " , dealing " + damage + " damage.";
   }
 
+  public String support(Adventurer other) {
+    int heal = 3;
+    other.setHP(other.getHP() + heal);
+    return this.getName() + " casts a heal on " + other.getName() + " , healing " + heal + " HP.";
+  }
+
+  public String support() {
+    int heal = 4;
+    this.setHP(this.getHP() + heal);
+    return this.getName() + " casts a heal on themselves, healing " + heal + " HP.";
+  }
+
+  public String specialAttack(Adventurer other) {
+    if (this.mana >= 5) {
+      int specialDamage = 5;
+      other.applyDamage(specialDamage);
+      this.mana -= 5;
+      return this.getName() + " chucks a stronger spell on " + other.getName() + " , dealing " + damage + " damage.";
+    } else {
+      return this.getName() + " doesn't have enoguh mana to cast a special attack.";
+    }
+  }
 
 }
