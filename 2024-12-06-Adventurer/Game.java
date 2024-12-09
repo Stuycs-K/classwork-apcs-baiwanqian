fdb53e6c94f3db92a520f852033cead8a84f663f
Christine Chen <zhzhnaicha2@gmail.com>
2024-12-09 10:59:52 -0500

    Started writing loop and user input + checks

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args) {
-    Adventurer player = new Mage(p1);
-    CodeWarrior enemy = new CodeWarrior(w1);
+
+    Adventurer p1 = new Mage("player");
+    CodeWarrior e1 = new CodeWarrior("enemy");
+
+    public static printStats(int )
+    for (int i = 0; i < 2; i++) {
+      System.out.println(this.getName() + this.getHP() + this.getSpecialName() +
+    }
+
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    String input = userInput.nextLine();
+    if (input != "attack" || "a" ||)
+    if (userInput.equals("quit")) {
+      System.out.println("Logging out...");
+      break;
+    }
+
   }
 }
