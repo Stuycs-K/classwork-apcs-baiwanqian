3f7715a5b4c95260567da31f4e293258b062edf3
Christine Chen <zhzhnaicha2@gmail.com>
2024-12-10 02:19:58 -0500

    completed while loop for user input, working on breaking/continuing program

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -3,22 +3,31 @@ import java.util.Scanner;
 public class Game {
   public static void main(String[] args) {
 
-    Adventurer p1 = new Mage("player");
-    CodeWarrior e1 = new CodeWarrior("enemy");
+    Adventurer player = new CodeWarrior("Player", 20, "Java");
+    Adventurer enemy = new Mage("Enemy");
 
-    public static printStats(int )
-    for (int i = 0; i < 2; i++) {
-      System.out.println(this.getName() + this.getHP() + this.getSpecialName() +
-    }
+    while (player.isAlive() && enemy.isAlive()) {
+      System.out.println(player.getName() + ": " + player.getHP() + "/" player.getMaxHP() +
+      " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName();
+      System.out.println(enemy.getName() + ": " + enemy.getHP() + "/" enemy.getMaxHP() +
+      " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName();
 
-    Scanner userInput = new Scanner(System.in);
-    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
-    String input = userInput.nextLine();
-    if (input != "attack" || "a" ||)
-    if (userInput.equals("quit")) {
-      System.out.println("Logging out...");
-      break;
-    }
-
+      Scanner userInput = new Scanner(System.in);
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      String input = userInput.nextLine().toLowerCase();
+      if (input.equals("quit")) {
+        System.out.println("Logging out...");
+        return;
+      }
+      String move;
+      if (input.equals("a")) || input.equals("attack") {
+        move = player.attack(enemy);
+      } else if (choice.equals("sp") || choice.equals("special")) {
+        move = player.specialAttack(enemy);
+      } else if (choice.equals("su") || choice.equals("support")) {
+        move = player.support();
+      } else {
+        System.out.println("Input Invalid! Try again.");
+      }
   }
 }
