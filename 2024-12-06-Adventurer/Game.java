d7fab13abfbe5618a4966059f5526d76f2988999
Christine Chen <zhzhnaicha2@gmail.com>
2024-12-10 02:29:58 -0500

    added 2 booleans for if to continue and if input is valid

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -2,32 +2,38 @@ import java.util.Scanner;
 
 public class Game {
   public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
 
     Adventurer player = new CodeWarrior("Player", 20, "Java");
     Adventurer enemy = new Mage("Enemy");
 
-    while (player.isAlive() && enemy.isAlive()) {
+    System.out.println("A");
+
+    boolean continue = true;
+
+    while (continue) {
       System.out.println(player.getName() + ": " + player.getHP() + "/" player.getMaxHP() +
       " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName();
       System.out.println(enemy.getName() + ": " + enemy.getHP() + "/" enemy.getMaxHP() +
       " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName();
 
-      Scanner userInput = new Scanner(System.in);
+      boolean validInput = true;
+
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
       String input = userInput.nextLine().toLowerCase();
+
       if (input.equals("quit")) {
         System.out.println("Logging out...");
-        return;
-      }
-      String move;
-      if (input.equals("a")) || input.equals("attack") {
-        move = player.attack(enemy);
+        continue = false;
+      } else if (input.equals("a")) || input.equals("attack") {
+        System.out.println(player.attack(enemy));
       } else if (choice.equals("sp") || choice.equals("special")) {
-        move = player.specialAttack(enemy);
+        System.out.println(move = player.specialAttack(enemy));
       } else if (choice.equals("su") || choice.equals("support")) {
-        move = player.support();
+        System.out.println(player.support());
       } else {
         System.out.println("Input Invalid! Try again.");
+        validInput = false;
       }
   }
 }
